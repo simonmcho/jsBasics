a987e30c3950c0276949706601e945f44d72f1c7
simonmcho <msc13@sfu.ca>
2016-03-10 14:55:20 -0800

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,8 @@
+//Reverse the provided string.
+
 function reverseString(str) {
-  var array1 = [];
-  var split = str.split("").reverse().join("");
-  return split;
+  str = str.split("").reverse().join("");
+  return str;
 }
 
-var entry = prompt("Enter a word you want to reverse");
-var result = reverseString(entry);  
-alert(result);
+reverseString("hello");
