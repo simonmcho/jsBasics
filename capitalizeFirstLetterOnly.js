a0c76b6d2b06a18f5c2a10b2a87b5c5fbc1dcc68
simonmcho <msc13@sfu.ca>
2016-03-14 11:02:47 -0700

    Update capitalizeFirstLetterOnly.js

--- capitalizeFirstLetterOnly.js
+++ capitalizeFirstLetterOnly.js
@@ -1,12 +1,12 @@
+//Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
 function titleCase(str) {
-  var retVal = str.split(" ");
-  var retArray = [];
-  
-  for (var i = 0; i < retVal.length; i++) {
-    retVal[i] = retVal[i].charAt(0).toUpperCase() + retVal[i].substr(1).toLowerCase();
+  var words = str.split(" ");
+  for (var i in words) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1).toLowerCase();
   }
-  retVal = retVal.join(" ");
-  return retVal;
+  words = words.join(" ");
+  return words;
 }
 
-titleCase(prompt("Enter in something"));
+titleCase("I'm a little tea pot");
