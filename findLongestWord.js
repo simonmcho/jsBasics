8f493ad226d4e62c6d272d7f9943c1a1dda5a340
simonmcho <msc13@sfu.ca>
2016-03-14 10:55:34 -0700

    Update findLongestWord.js

--- findLongestWord.js
+++ findLongestWord.js
@@ -1,3 +1,5 @@
+//Return the length of the longest word in the provided sentence.
+
 var word = prompt("Type in a sentence and find out how many characters the longest word has");
 function findLongestWord(word) {
   var split = word.split(" ");
@@ -8,6 +10,17 @@ function findLongestWord(word) {
     }
   }
   alert(longestWord);
+}
+
+function findLongestWord(str) {
+  str = str.split(" ");
+  var length = 0;
+  for (var i in str) {
+    if (str[i].length > length) {
+      length = str[i].length;
+    }
+  }
+  return length;
 }
 
 findLongestWord(word);
