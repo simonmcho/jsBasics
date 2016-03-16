a3b75fb1a310e4c6a0a970572d2a738f4429671a
simonmcho <msc13@sfu.ca>
2016-03-16 16:54:43 -0700

    Update mutationsBasics.js

--- mutationsBasics.js
+++ mutationsBasics.js
@@ -1,3 +1,5 @@
+//Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
 function mutation(arr) {
   var firstEntry = arr[0].toLowerCase().split("");
   var secondEntry = arr[1].toLowerCase().split("");
@@ -20,3 +22,31 @@ function mutation(arr) {
 }
 
 mutation(["Alien", "liNE"]);
+
+/*Return a MultiDimensional Array so that the first Array includes all the characters that are included in both indices of mutation() 
+And the second array includes all the characters that are only in the second index of mutation()
+*/
+
+function mutation(arr) {
+  var firstEntry = arr[0].toLowerCase();
+  var secondEntry = arr[1].toLowerCase();
+  var number = 0;
+  var included = [];
+  var excluded = [];
+  var finalArray = [];
+  
+  for (var i in secondEntry) {
+    if (firstEntry.indexOf(secondEntry[i]) > -1) {
+      included.push(secondEntry[i]);
+    }
+    else {
+      excluded.push(secondEntry[i]);
+    }
+  }
+  
+  finalArray.push(included, excluded);
+  
+  return finalArray;
+}
+
+mutation(["hello", "neo"]);
